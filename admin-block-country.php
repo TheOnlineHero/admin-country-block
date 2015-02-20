2d1cef83166746f68b4a934602dcc798a800a7e4
TheOnlineHero <TheOnlineHero2012@gmail.com>
2015-02-20 21:03:28 +0800

    Latest version from SVN.

--- admin-block-country.php
+++ admin-block-country.php
@@ -6,11 +6,9 @@ Description: Blocks admin site by country.
 
 Installation:
 
-1) Install WordPress 3.5.2 or higher
+1) Install WordPress 3.9 or higher
 
 2) Download the latest from:
-
-http://wordpress.org/extend/plugins/tom-m8te 
 
 http://wordpress.org/extend/plugins/admin-block-country
 
@@ -18,132 +16,141 @@ http://wordpress.org/extend/plugins/admin-block-country
 
 4) Activate the plugin.
 
-Version: 1.0
+Version: 6.0
 Author: TheOnlineHero - Tom Skroza
 License: GPL2
 */
+
+if (!function_exists("geoip_open")) {
+  include_once("geoip-api-php-master/geoip.inc");
+  include_once("geoip-api-php-master/geoipcity.inc");
+  include_once("geoip-api-php-master/geoipregionvars.php");
+}
 include_once("get-country-name.php");
-
-
-function are_admin_block_country_dependencies_installed() {
-  return is_plugin_active("tom-m8te/tom-m8te.php") && is_plugin_active("jquery-ui-theme/jquery-ui-theme.php");
+if (!class_exists("ABCTomM8")) {
+  include_once("lib/tom-m8te.php");
 }
 
-add_action( 'admin_notices', 'admin_block_country_notice_notice' );
-function admin_block_country_notice_notice(){
-  $activate_nonce = wp_create_nonce( "activate-admin-block-country-dependencies" );
-  $tom_active = is_plugin_active("tom-m8te/tom-m8te.php");
-  if (!($tom_active)) { ?>
-    <div class='updated below-h2'><p>Before you can use Admin Block Country, please install/activate the following plugin:</p>
-    <ul>
-      <?php if (!$tom_active) { ?>
-        <li>
-          <a target="_blank" href="http://wordpress.org/extend/plugins/tom-m8te/">Tom M8te</a> 
-           &#8211; 
-          <?php if (file_exists(ABSPATH."/wp-content/plugins/tom-m8te/tom-m8te.php")) { ?>
-            <a href="<?php echo(get_option("siteurl")); ?>/wp-admin/?admin_block_country_install_dependency=tom-m8te&_wpnonce=<?php echo($activate_nonce); ?>">Activate</a>
-          <?php } else { ?>
-            <a href="<?php echo(get_option("siteurl")); ?>/wp-admin/plugin-install.php?tab=plugin-information&plugin=tom-m8te&_wpnonce=<?php echo($activate_nonce); ?>&TB_iframe=true&width=640&height=876">Install</a> 
-          <?php } ?>
-        </li>
-      <?php } ?>
-    </ul>
-    </div>
-    <?php
-  }
-
-}
-
-add_action( 'admin_init', 'register_admin_block_country_install_dependency_settings' );
-function register_admin_block_country_install_dependency_settings() {
-  if (isset($_GET["admin_block_country_install_dependency"])) {
-    if (wp_verify_nonce($_REQUEST['_wpnonce'], "activate-admin-block-country-dependencies")) {
-      switch ($_GET["admin_block_country_install_dependency"]) {
-        case 'tom-m8te':  
-          activate_plugin('tom-m8te/tom-m8te.php', 'plugins.php?error=false&plugin=tom-m8te.php');
-          wp_redirect(get_option("siteurl")."/wp-admin/admin.php?page=admin-block-country/admin-block-country.php");
-          exit();
-          break;   
-        default:
-          throw new Exception("Sorry unable to install plugin.");
-          break;
-      }
-    } else {
-      die("Security Check Failed.");
-    }
-  }
-}
-
-
-add_action( 'admin_init', 'register_admin_block_country_block_by_country' );
+add_action( 'init', 'register_admin_block_country_block_by_country' );
 function register_admin_block_country_block_by_country() {
-  if (are_admin_block_country_dependencies_installed()) {
+  
+  if (preg_match("/wp-login|wp-admin/", ABCTomM8::get_current_url())) {
     $my_country = admin_block_country_get_country_from_ip($_SERVER['REMOTE_ADDR']);
-    foreach(admin_block_country_current_countries_blocked() as $country) {
-      if ($country == $my_country) {
-        exit;
+    if ($my_country != "") {
+      foreach(admin_block_country_current_countries_blocked() as $country) {
+        if ($country == $my_country) {
+          exit;
+        }
       }
-    } 
-  }
+    }
+  } 
+  
 }
 
 add_action( 'admin_init', 'register_admin_block_country_settings' );
 function register_admin_block_country_settings() {
   register_setting( 'admin-block-country-settings-group', 'admin_block_country_list' );
+  register_setting( 'admin-block-country-settings-group', 'admin_block_country_method' );
 }
 
 add_action('admin_menu', 'register_admin_block_country_page');
 function register_admin_block_country_page() {
-  if (are_admin_block_country_dependencies_installed()) {
-	  add_menu_page('Block Country', 'Block Country', 'manage_options', 'admin-block-country/admin-block-country.php', 'admin_block_country_initial_page');
-  }
+  add_menu_page('Block Country', 'Block Country', 'manage_options', 'admin-block-country/admin-block-country.php', 'admin_block_country_initial_page');
 }
 
 function admin_block_country_initial_page() {
-  if (are_admin_block_country_dependencies_installed()) {
-    wp_register_script("admin_block_countries", plugins_url("/js/application.js", __FILE__));
-    wp_enqueue_script("admin_block_countries");
-  
-    wp_register_style("admin_block_countries", plugins_url("/css/style.css", __FILE__));
-    wp_enqueue_style("admin_block_countries");
-  
-    if ($_POST["action"] == "Block") {
-      update_option("admin_block_country_list", tom_get_query_string_value("block_countries"));
+  wp_register_script("admin_block_countries", plugins_url("/js/application.js", __FILE__));
+  wp_enqueue_script("admin_block_countries");
+
+  wp_register_style("admin_block_countries", plugins_url("/css/style.css", __FILE__));
+  wp_enqueue_style("admin_block_countries");
+
+  if ($_POST["action"] == "Upload") {
+    $abc_verify_upload = wp_verify_nonce($_REQUEST["_wpnonce"], "abc-upload-abc");
+
+    $allowedExts = array("dat");
+    $temp = explode(".", $_FILES["block_country_upload_file"]["name"][0]);
+    $extension = end($temp);
+    if ($abc_verify_upload && $_FILES["block_country_upload_file"]["name"][0] == "GeoLiteCity.dat" && in_array($extension, $allowedExts)) {
+      move_uploaded_file($_FILES["block_country_upload_file"]["tmp_name"][0],
+          ABSPATH."/wp-content/uploads/".$_FILES["block_country_upload_file"]["name"][0]);
+      echo("<div class='updated below-h2'><p>Upload was a success.</p></div>");
+    } else {
+      echo("<div class='updated below-h2'><p>Invalid upload, can only upload GeoLityCity.dat file.</p></div>");
     }
-    foreach(admin_block_country_current_countries_blocked() as $country) {
-      $i=0;
-      foreach(admin_block_country_checkbox_list() as $key => $value) {
-        if (str_replace(" ", "", $key) == str_replace(" ", "", $country)) {
-            $_POST["block_countries_".$i] = $country;
-        }
-        $i++;
-      }
-    }
-  
-    ?>
-    <div class="postbox " style="display: block; ">
-    <div class="inside">
-      <?php 
-      global $geoipcountry;
-      $my_country = admin_block_country_get_country_from_ip($_SERVER['REMOTE_ADDR']); ?>
-      <p>Please choose which countries you wish to exclude from accessing the admin site.</p>
-      <?php if ($my_country != "ZZ") { ?>
-            <p>You cannot block your own country, for your own protect. <?php echo($geoipcountry[$my_country]); ?> has been taken off the list.</p>
-      <?php } ?>
-      <form action="" method="post" id="block_countries_form">
-        <fieldset>
-        <?php 
-        tom_add_form_field(null, "checkbox", "Block Countries", "block_countries", "block_countries", array(), "p", array("class" => "checkbox"), admin_block_country_checkbox_list()); ?>
-        </fieldset>
-        <fieldset class="submit">
-          <p><input type="submit" name="action" value="Block"/></p>
-        </fieldset>
-      </form>
-    </div>
-    </div>
-  
-    <?php
+  } else if ($_POST["action"] == "Update") {
+    update_option("admin_block_country_method", $_POST["block_country_method"]);
+    echo("<div class='updated below-h2'><p>Service Updated</p></div>");
+  } else if ($_POST["action"] == "Block") {
+    update_option("admin_block_country_list", ABCTomM8::get_query_string_value("block_countries"));
+    echo("<div class='updated below-h2'><p>Admin Block Was Successful.</p></div>");
   }
+  foreach(admin_block_country_current_countries_blocked() as $country) {
+    $i=0;
+    foreach(admin_block_country_checkbox_list() as $key => $value) {
+      if (str_replace(" ", "", $key) == str_replace(" ", "", $country)) {
+          $_POST["block_countries_".$i] = $country;
+      }
+      $i++;
+    }
+  }
+
+  ?>
+  <div class="postbox " style="display: block; ">
+  <div class="inside">
+    <p>Admin Block Country is now different. You can now upload a local ip database that Admin Block Country can use. Please download the database from:</p>
+    <p><a target="_blank" href="http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz">http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz</a></p>
+    <p>Unzip it on your local computer and then upload the unzipped GeoLiteCity.dat file.</p>
+    <form action="" method="post" id="block_countries_upload" enctype="multipart/form-data">
+      <?php
+      $abc_upload_abc_nonce = wp_create_nonce( "abc-upload-abc" );
+      ?>
+      <input type='hidden' name='_wpnonce' value='<?php echo($abc_upload_abc_nonce); ?>'/>
+      <?php
+        ABCTomM8::add_form_field(null, "file", "Upload GeoLiteCity.dat", "block_country_upload_file", "block_country_upload_file", array(), "p", array("class" => "file"));
+      ?>
+      <p><input type="submit" name="action" value="Upload"/></p>
+    </form>
+    <br/>
+    <br/>
+    <p>Please choose a backup external IP to Country service. If one fails try the other.</p>
+    <form action="" method="post" id="block_countries_setting">
+    <?php
+      if (get_option("admin_block_country_method") == "") {
+        update_option("admin_block_country_method", "1");
+      }
+      $_GET["block_country_method"] = get_option("admin_block_country_method");
+      ABCTomM8::add_form_field(null, "radio", "IP to Country Service", "block_country_method", "block_country_method", array(), "p", array("class" => "radio"), array("2" => "Use geoplugin.net IP to Country service - http://www.geoplugin.net", "3" => "Use marketingmix IP to Country service - http://ipcountry.marketingmix.com.au", "4" => "Use utrace IP to Country service - http://xml.utrace.de"));
+    ?>
+    <p><input type="submit" name="action" value="Update"/></p>
+    
+    </form>
+  </div>
+  </div>
+  <div class="postbox " style="display: block; ">
+  <div class="inside">
+    <?php 
+    global $geoipcountry;
+    $my_country = admin_block_country_get_country_from_ip($_SERVER['REMOTE_ADDR']); ?>
+    <p>Please choose which countries you wish to exclude from accessing the admin site.</p>
+    <?php if ($my_country != "ZZ") { ?>
+          <p>You cannot block your own country, for your own protection. <?php echo($geoipcountry[$my_country]); ?> has been taken off the list.</p>
+    <?php } ?>
+    <form action="" method="post" id="block_countries_form">
+      <fieldset>
+      <?php 
+      ABCTomM8::add_form_field(null, "checkbox", "Block Countries", "block_countries", "block_countries", array(), "p", array("class" => "checkbox"), admin_block_country_checkbox_list()); ?>
+      </fieldset>
+      <fieldset class="submit">
+        <p><input type="submit" name="action" value="Block"/></p>
+      </fieldset>
+    </form>
+  </div>
+  </div>
+  
+  <?php
+
+  ABCTomM8::add_social_share_links("http://wordpress.org/extend/plugins/admin-block-country/");
 }
 
 function admin_block_country_current_countries_blocked() {
--- readme.txt
+++ readme.txt
@@ -1,10 +1,10 @@
 === Admin Block Country ===
-Contributors: MMDeveloper
+Contributors: MMDeveloper, The Marketing Mix Osborne Park Perth
 Donate link: 
 Tags: security, block, country
 Requires at least: 3.3
-Tested up to: 3.5.2
-Stable tag: 1.0
+Tested up to: 4.0
+Stable tag: 6.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -12,30 +12,101 @@ Block access to your admin pages by country.
 
 == Description ==
 
-Easy to use plugin, that blocks access to your wp-admin area by country.
+Easy to use plugin, that blocks access to your wp-admin area by country. Uses geoip-api-php as the library to work out the visitor's country.
+
+Built by The Marketing Mix Perth: http://www.marketingmix.com.au
 
 == Installation ==
 
-1) Install WordPress 3.5 or higher
+1) Install WordPress 4.0 or higher
 
 2) Download the latest from:
 
-http://wordpress.org/extend/plugins/tom-m8te 
-
-http://wordpress.org/extend/plugins/a-forms
+http://wordpress.org/extend/plugins/admin-block-country
 
 3) Login to WordPress admin, click on Plugins / Add New / Upload, then upload the zip file you just downloaded.
 
 4) Activate the plugin.
 
 
+Built by The Marketing Mix Perth: http://www.marketingmix.com.au
+
+
 == Changelog ==
+
+= 6.0 =
+
+* I removed Who country service. You can now upload your own local ip database from maxmind. I've added in the instructions.
+
+= 5.2 =
+
+* New IP to country service - utrace. Example http://xml.utrace.de/?query=183.60.244.29
+
+= 5.0 =
+
+* Removed Tom M8te dependency.
+
+= 4.3 =
+
+* Fixed bug with Admin "Select All" checkbox. Played havac with service selector. Never noticed it before.
+
+= 4.2 =
+
+* Fixed bug with geoplugin.
+
+= 4.1 =
+
+* Added in ipcountry.marketingmix.com.au ip to country service which is a server that I own. I noticed that the existing 2 have failed atleast once.
+
+= 4.0 =
+
+* Tried Maxmind in version 3.0, but 2 of my clients couldn't use it, so I've ditched it and now I use two external services: http://who.is, http://www.geoplugin.net.
+
+= 3.0 =
+
+* Uses another method for discovering the country of an ip address. 
+
+= 2.0 =
+
+* Used a different method for discovering the country of an ip address. Seems to be less memory intensive.
 
 = 1.0 =
 
 * Initial Commit
 
 == Upgrade notice ==
+
+= 6.0 =
+
+* I removed Who country service. You can now upload your own local ip database from maxmind. I've added in the instructions.
+
+= 5.0 =
+
+* Removed Tom M8te dependency.
+
+= 4.3 =
+
+* Fixed bug with Admin "Select All" checkbox. Played havac with service selector. Never noticed it before.
+
+= 4.2 =
+
+* Fixed bug with geoplugin.
+
+= 4.1 =
+
+* Added in ipcountry.marketingmix.com.au ip to country service which is a server that I own. I noticed that the existing 2 have failed atleast once.
+
+= 4.0 =
+
+* Tried Maxmind in version 3.0, but 2 of my clients couldn't use it, so I've ditched it and now I use two external services: http://who.is, http://www.geoplugin.net.
+
+= 3.0 =
+
+* Uses another method for discovering the country of an ip address. 
+
+= 2.0 =
+
+* Used a different method for discovering the country of an ip address. Seems to be less memory intensive.
 
 = 1.0 =
 
